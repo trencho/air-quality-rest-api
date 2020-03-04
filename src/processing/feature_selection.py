fd067a45e9ea178da39716ec27055e64f3d19ab9
trencho <atrenchevski@gmail.com>
2020-03-04 09:56:35 +0100

    Removed unused method attributes, optimized imports

--- src/processing/feature_selection.py
+++ src/processing/feature_selection.py
@@ -3,18 +3,17 @@ import statsmodels.api as sm
 
 
 def backward_elimination(X, y):
-    cols = list(X.columns)
-    while len(cols) > 0:
-        X_1 = X[cols]
-        X_1 = sm.add_constant(X_1, has_constant='add')
-        model = sm.OLS(y, X_1).fit()
-        p = pd.Series(model.pvalues.values[1:], index=cols)
+    features = list(X.columns)
+    while len(features) > 0:
+        features_with_constant = sm.add_constant(X[features], has_constant='add')
+        model = sm.OLS(y, features_with_constant).fit()
+        p = pd.Series(model.pvalues.values[1:], index=features)
         pmax = max(p)
         feature_with_p_max = p.idxmax()
         if pmax > 0.05:
-            cols.remove(feature_with_p_max)
+            features.remove(feature_with_p_max)
         else:
             break
-    selected_features = cols
+    selected_features = features
 
     return selected_features
