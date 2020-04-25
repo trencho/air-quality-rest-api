543ae68ce99eabf2b0a831b5f0e94772fef19b3d
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-04-25 02:29:18 +0200

    Fixed imports

--- src/api/app.py
+++ src/api/app.py
@@ -1,6 +1,8 @@
 from api.config import create_app
+from api.resources import register_blueprints
 
 app = create_app()
+register_blueprints(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
