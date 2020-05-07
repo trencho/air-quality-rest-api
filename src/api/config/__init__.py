d67f6cc84b816b246c3a784937f5c8b1f8e58d21
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-05-07 10:48:14 +0200

    Changed the name of the routes to be blueprints accordingly, moved the blueprint registration in the upper level where are all initializations are done, added a comment for starting the app in debug mode

--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -1,13 +1,17 @@
+import os
+
 from flask import Flask
 
+from .blueprints import register_blueprints
 from .db import mongo
-from .routes import register_blueprints
 from .swagger import swagger
 
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+
+    register_blueprints(app)
+    # Comment the 3 lines for the mongodb when running app in debug mode
     app.config['MONGO_URI'] = ('mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD']
                                + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE'])
     mongo.init_app(app)
