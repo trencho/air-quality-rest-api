f3dc0c8a1c7bcbc559d9fdf9d1ae0d8eb887ee0b
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-05-07 10:48:14 +0200

    Changed the name of the routes to be blueprints accordingly, moved the blueprint registration in the upper level where are all initializations are done, added a comment for starting the app in debug mode

--- src/api/app.py
+++ src/api/app.py
@@ -1,7 +1,6 @@
-from api.config import create_app, register_blueprints
+from api.config import create_app
 
 app = create_app()
-register_blueprints(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
