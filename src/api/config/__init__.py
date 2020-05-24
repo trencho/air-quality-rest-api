bca2f1f14ef8ebc8cdb662b2a1ee159d16d3d643
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-05-24 19:15:57 +0200

    Adjusted mongo connection for heroku deployment

--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -11,9 +11,10 @@ def create_app():
     app = Flask(__name__)
 
     register_blueprints(app)
-    # Comment the 3 lines for the mongodb when running app in debug mode
-    app.config['MONGO_URI'] = ('mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD']
-                               + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE'])
+    # Comment the 4 lines for the mongodb when running app in debug mode
+    app.config['MONGO_URI'] = ('mongodb+srv://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD']
+                               + '@' + os.environ['MONGODB_HOSTNAME'] + '/' + os.environ['MONGODB_DATABASE']
+                               + '?retryWrites=true&w=majority')
     mongo.init_app(app)
 
     app.config['SWAGGER'] = {
