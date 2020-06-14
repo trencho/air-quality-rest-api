df72cca9c9fa6f2fc7d04e15d4a1d1f26b32ce1a
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-06-14 14:29:02 +0200

    Added database and swagger configuration functions

--- src/api/config/db.py
+++ src/api/config/db.py
@@ -1,3 +1,15 @@
+from os import environ
+
 from flask_pymongo import PyMongo
 
+from definitions import mongo_db_host_env_value, mongo_db_name_env_value, mongo_db_user_name_env_value, \
+    mongo_db_user_pass_env_value
+
 mongo = PyMongo()
+
+
+def configure_database(app):
+    app.config['MONGO_URI'] = ('mongodb+srv://' + environ.get(mongo_db_user_name_env_value) + ':'
+                               + environ.get(mongo_db_user_pass_env_value) + '@' + environ.get(mongo_db_host_env_value)
+                               + '/' + environ.get(mongo_db_name_env_value) + '?retryWrites=true&w=majority')
+    mongo.init_app(app)
