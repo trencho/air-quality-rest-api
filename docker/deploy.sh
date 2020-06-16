860f437f46b7e239509abb23105b0bdad1ae2421
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-06-17 01:01:07 +0200

    Moved dockerhub deploy shell script in docker directory, added additional parameter for building a specific Dockerfile, adjusted deprecated maintainer instruction

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
+  echo 'You must specify a Dockerfile!'
+  exit 1
+else
+  dockerfile=$1
+fi
+if [ -z "$2" ]; then
   echo 'You must specify a Docker Hub username!'
   exit 1
 else
-  username=$1
+  username=$2
 fi
-if [ -z "$2" ]; then
+if [ -z "$3" ]; then
   echo 'Missing tag name, setting to "prototype"'
   tag_name=prototype
 else
-  tag_name=$2
+  tag_name=$3
 fi
-if [ -z "$3" ]; then
+if [ -z "$4" ]; then
   echo 'Missing tag number, setting to "latest"'
   tag_number=latest
 else
-  tag_number=$3
+  tag_number=$4
 fi
 
-docker build -f ./Dockerfile -t "${tag_name}":"${tag_number}" .
+docker build -f "${dockerfile}" -t "${tag_name}":"${tag_number}" .
 docker tag "${tag_name}":"${tag_number}" "${username}"/"${tag_name}"
 docker push "${username}"/"${tag_name}"
