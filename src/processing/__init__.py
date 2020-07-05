53ced80d91a5c77be16d8cc3b212270e3d3263a0
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-05 22:10:55 +0200

    Moved the method for overwritting previous values in a separate file

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -2,3 +2,4 @@ from .feature_generation import generate_features, encode_categorical_data
 from .feature_scaling import value_scaling
 from .feature_selection import backward_elimination
 from .merge_data import merge_air_quality_data
+from .modify_data import previous_value_overwrite
