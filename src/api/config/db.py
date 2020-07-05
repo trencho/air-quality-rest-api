3dc0528078d1b6e5bfdf543ee7303254036479e0
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-05 20:39:19 +0200

    Renamed the mongodb environment variables for configuring the database

--- src/api/config/db.py
+++ src/api/config/db.py
@@ -2,14 +2,14 @@ from os import environ
 
 from flask_pymongo import PyMongo
 
-from definitions import mongo_db_host_env_value, mongo_db_name_env_value, mongo_db_user_name_env_value, \
-    mongo_db_user_pass_env_value
+from definitions import mongodb_host_env_value, mongodb_name_env_value, mongodb_user_name_env_value, \
+    mongodb_user_pass_env_value
 
 mongo = PyMongo()
 
 
 def configure_database(app):
-    app.config['MONGO_URI'] = ('mongodb+srv://' + environ.get(mongo_db_user_name_env_value) + ':'
-                               + environ.get(mongo_db_user_pass_env_value) + '@' + environ.get(mongo_db_host_env_value)
-                               + '/' + environ.get(mongo_db_name_env_value) + '?retryWrites=true&w=majority')
+    app.config['MONGO_URI'] = ('mongodb+srv://' + environ.get(mongodb_user_name_env_value) + ':'
+                               + environ.get(mongodb_user_pass_env_value) + '@' + environ.get(mongodb_host_env_value)
+                               + '/' + environ.get(mongodb_name_env_value) + '?retryWrites=true&w=majority')
     mongo.init_app(app)
