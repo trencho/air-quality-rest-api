dc4493a395a5452a94b9cdcf35ff68776ab0ca9a
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-05 22:10:55 +0200

    Moved the method for overwritting previous values in a separate file

--- src/processing/modify_data.py
+++ src/processing/modify_data.py
@@ -1,24 +1,6 @@
-from numpy import int64
-from pandas import DataFrame, read_csv, to_datetime
+def previous_value_overwrite(dataframe):
+    dataframe = dataframe.shift(periods=-1, axis=0)
+    dataframe.reset_index(drop=True, inplace=True)
+    dataframe.drop(len(dataframe) - 1, inplace=True)
 
-data_frame = DataFrame()
-
-for year in range(2015, 2019):
-    df = read_csv('D:/Downloads/NAPMMU/Datasets/Pollution/measurements_' + str(year) + '.csv', sep=';')
-    df.rename(columns={'DateTime': 'time'}, inplace=True)
-
-    if year == 2018:
-        formatTime = '%Y-%m-%d %H:%M:%S'
-    else:
-        formatTime = '%d/%m/%Y %H:%M'
-
-    df['time'] = to_datetime(df['time'], format=formatTime).astype(int64) // 10 ** 9
-
-    data_frame = data_frame.append(df, sort=True)
-
-station_names = ['Centar', 'GaziBaba', 'Karpos', 'Kavadarci', 'Kicevo', 'Kocani', 'Kumanovo', 'Lisice', 'Miladinovci',
-                 'Tetovo']
-for station_name in station_names:
-    df = data_frame[data_frame['StationName'] == station_name]
-    df.drop(columns='StationName', inplace=True)
-    df.to_csv('D:/Downloads/NAPMMU/Datasets/Pollution/pollution_report_' + station_name + '.csv', index=False)
+    return dataframe
