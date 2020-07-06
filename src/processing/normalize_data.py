63c5a307c8d98475071a7ad918ce926fa812ebc1
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-06 23:01:39 +0200

    Added method for flattening a list of nested dictionaries

--- src/processing/normalize_data.py
+++ src/processing/normalize_data.py
@@ -2,6 +2,31 @@ from numpy import int64
 from pandas import DataFrame, merge as pandas_merge, to_datetime
 
 
+def flatten_json(nested_json: dict, exclude=None) -> dict:
+    """
+    Flatten a list of nested dicts.
+    """
+    if exclude is None:
+        exclude = ['']
+    out = dict()
+
+    def flatten(x: (list, dict, str), name: str = '', exclude=exclude):
+        if type(x) is dict:
+            for a in x:
+                if a not in exclude:
+                    flatten(x[a], f'{name}{a}_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, f'{name}{i}_')
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(nested_json)
+    return out
+
+
 def current_hour(t):
     return t.replace(microsecond=0, second=0, minute=0, hour=t.hour)
 
