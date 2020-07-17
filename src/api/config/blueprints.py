aa84c29cac7aafdf320c8df368714f70050e315a
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-17 10:27:28 +0200

    Disabled fetch and train endpoints for production deployment

--- src/api/config/blueprints.py
+++ src/api/config/blueprints.py
@@ -1,19 +1,17 @@
 from api.blueprints.cities import cities
-from api.blueprints.fetch import fetch
 from api.blueprints.forecast import forecast
 from api.blueprints.history import history
 from api.blueprints.pollutants import pollutant
 from api.blueprints.sensors import sensors
-from api.blueprints.train import train
 
 __all__ = [
     'cities',
-    'fetch',
+    # 'fetch',
     'forecast',
     'history',
     'pollutant',
     'sensors',
-    'train'
+    # 'train'
 ]
 
 
