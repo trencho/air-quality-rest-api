d7a021e27bc5445633ece968ce1831eca7e057fa
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-18 00:48:21 +0200

    Fixed the automated data-dump for dumping png files, changed the time format of the data-dump

--- src/api/config/git.py
+++ src/api/config/git.py
@@ -18,18 +18,32 @@ def append_commit_files(file_list, file_names, root, data, file):
 
 
 def merge_csv_files(repo_name, file_name, data):
-    repo = g.get_user().get_repo(repo_name)
-    repo_file = repo.get_contents(file_name)
-    repo_file_content = read_csv(BytesIO(repo_file.decoded_content))
     local_file_content = read_csv(StringIO(data))
-    repo_file_content = repo_file_content.append(local_file_content, ignore_index=True, sort=True)
-    repo_file_content.drop_duplicates(inplace=True)
-    return repo_file_content.to_csv(index=False)
+    try:
+        repo = g.get_user().get_repo(repo_name)
+        repo_file = repo.get_contents(file_name)
+        repo_file_content = read_csv(BytesIO(repo_file.decoded_content))
+        local_file_content = local_file_content.append(repo_file_content, ignore_index=True, sort=True)
+    except:
+        pass
+    local_file_content.drop_duplicates(inplace=True)
+    return local_file_content.to_csv(index=False)
+
+
+def commit_git_files(repo, element_list, base_tree, master_sha, commit_message, master_ref):
+    try:
+        tree = repo.create_git_tree(element_list, base_tree)
+        parent = repo.get_git_commit(master_sha)
+        commit = repo.create_git_commit(commit_message, tree, [parent])
+        master_ref.edit(commit.sha)
+    except:
+        commit_git_files(repo, element_list[:len(element_list) // 2], base_tree, master_sha, commit_message, master_ref)
+        commit_git_files(repo, element_list[len(element_list) // 2:], base_tree, master_sha, commit_message, master_ref)
 
 
 def update_git_files(file_names, file_list, repo_name, branch, commit_message=''):
     if commit_message == '':
-        commit_message = f'Data Updated - {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}'
+        commit_message = f'Data Updated - {datetime.now().strftime("%H:%M:%S %d-%m-%Y")}'
 
     repo = g.get_user().get_repo(repo_name)
     master_ref = repo.get_git_ref(f'heads/{branch}')
@@ -38,11 +52,13 @@ def update_git_files(file_names, file_list, repo_name, branch, commit_message=''
     element_list = []
     for i in range(0, len(file_list)):
         if file_names[i].endswith('.csv'):
-            file_list[i] = merge_csv_files(repo, file_names[i], file_list[i])
-        element = InputGitTreeElement(file_names[i], '100644', 'blob', file_list[i])
-        element_list.append(element)
-    tree = repo.create_git_tree(element_list, base_tree)
-    parent = repo.get_git_commit(master_sha)
-    commit = repo.create_git_commit(commit_message, tree, [parent])
-    master_ref.edit(commit.sha)
+            file_list[i] = merge_csv_files(repo_name, file_names[i], file_list[i])
+            element = InputGitTreeElement(file_names[i], '100644', 'blob', file_list[i])
+            element_list.append(element)
+        elif file_names[i].endswith('.png'):
+            file_list[i] = repo.create_git_blob(file_list[i].decode(), 'base64')
+            element = InputGitTreeElement(file_names[i], '100644', 'blob', sha=file_list[i].sha)
+            element_list.append(element)
+
+    commit_git_files(repo, element_list, base_tree, master_sha, commit_message, master_ref)
     print('Update complete')
