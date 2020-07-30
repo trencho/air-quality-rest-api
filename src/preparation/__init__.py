d1170d7068476e648d0dec77005a20b65662fbf0
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-07-31 00:12:05 +0200

    Added caching of cities and sensors for optimizing performance, added flask-caching in requirements.txt

--- src/preparation/__init__.py
+++ src/preparation/__init__.py
@@ -1,4 +1,4 @@
 from .handle_data import save_dataframe
-from .location_data import check_city, check_sensor, fetch_cities, fetch_locations, fetch_sensors
+from .location_data import check_city, check_sensor, fetch_cities, fetch_sensors
 from .pollution_data import fetch_pollution_data
 from .weather_data import fetch_weather_data
