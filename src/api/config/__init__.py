c37230adc5a23c395f212c4ebefd7d69d817f653
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-12-15 17:25:13 +0100

    Adjusted the comments for operations executed on application startup, included method for training regression models for all available locations during startup

--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -4,13 +4,13 @@ from .blueprints import register_blueprints
 from .cache import configure_cache
 from .database import configure_database
 from .environment import check_environment_variables, fetch_db_data
-from .schedule import schedule_jobs
+from .schedule import model_training, schedule_jobs
 from .swagger import configure_swagger
 
 
 def create_app():
-    # Comment these 2 lines to skip the environment variable check and scheduling of api operations when running app in
-    # debug mode
+    # Comment these 2 lines to skip the environment variable check and scheduling of api operations when running
+    # application in debug mode
     check_environment_variables()
     schedule_jobs()
 
@@ -20,9 +20,12 @@ def create_app():
 
     configure_cache(app)
 
-    # Comment these 2 lines to skip the mongodb configuration when running app in debug mode
+    # Comment these 2 lines to skip the mongodb configuration when running application in debug mode
     configure_database(app)
     fetch_db_data()
+
+    # Comment this line to skip training regression models for all available locations during application startup
+    model_training()
 
     configure_swagger(app)
 
