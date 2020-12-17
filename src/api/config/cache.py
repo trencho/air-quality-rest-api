8621cd195e48055701e9dc17e88cf5bddfe6c97d
Aleksandar Trenchevski <atrenchevski@gmail.com>
2020-12-17 17:28:04 +0100

    Removed the cache threshold in order to prevent removing necessary cache data

--- src/api/config/cache.py
+++ src/api/config/cache.py
@@ -5,8 +5,9 @@ cache = Cache()
 
 def configure_cache(app):
     config = {
-        'CACHE_DEFAULT_TIMEOUT': 0,
         'CACHE_TYPE': 'filesystem',
+        'CACHE_DEFAULT_TIMEOUT': 0,
+        'CACHE_THRESHOLD': 0,
         'CACHE_DIR': '/tmp/cache'
     }
     cache.init_app(app, config)
