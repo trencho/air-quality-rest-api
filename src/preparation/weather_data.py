326258c84f9f104bc0505032b2c29a8c375c97f5
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-03-20 23:28:32 +0100

    Migrated the methods for fetching weather data from the Dark Sky API to OpenWeather API

--- src/preparation/weather_data.py
+++ src/preparation/weather_data.py
@@ -4,41 +4,37 @@ from traceback import format_exc
 from pandas import DataFrame, json_normalize
 from requests import get as requests_get
 
-from definitions import DATA_EXTERNAL_PATH, dark_sky_token
+from definitions import DATA_EXTERNAL_PATH, open_weather_token
+from processing import flatten_json
 from .handle_data import save_dataframe
 
 day_in_seconds = 86400
 
 
 def fetch_weather_data(city_name, sensor, start_time, end_time):
-    domain = 'https://api.darksky.net'
-    token = environ[dark_sky_token]
-    url = f'{domain}/forecast/{token}/{sensor["position"]},{start_time}'
-    exclude = 'currently,minutely,daily,alerts,flags'
-    extend = 'hourly'
-    params = f'exclude={exclude}&extend={extend}'
+    url = 'https://api.openweathermap.org/data/2.5/onecall'
+    sensor_position = sensor['position'].split(',')
+    lat, lon = float(sensor_position[0]), float(sensor_position[1])
+    units = 'metric'
+    exclude = 'current,minutely,daily'
+    token = environ[open_weather_token]
+    params = f'lat={lat}&lon={lon}&units={units}&exclude={exclude}&appid={token}'
 
     dataframe = DataFrame()
     while start_time < end_time:
         weather_response = requests_get(url=url, params=params)
         try:
             weather_json = weather_response.json()
-            hourly = weather_json['hourly']
-            df = json_normalize(hourly['data'])
-            df.sort_values(by='time', inplace=True)
-            last_timestamp = df['time'].iloc[-1]
-            if start_time < last_timestamp:
-                start_time = last_timestamp
-            else:
-                start_time += day_in_seconds
+            hourly_data = weather_json['hourly']
+            df = json_normalize([flatten_json(hourly) for hourly in hourly_data])
+            df.rename(columns={'dt': 'time'}, inplace=True, errors='ignore')
             dataframe = dataframe.append(df, ignore_index=True)
         except (KeyError, ValueError):
             print(weather_response)
             print(format_exc())
-            start_time += day_in_seconds
 
-        url = f'{domain}/forecast/{token}/{sensor["position"]},{start_time}'
-
+    dataframe.sort_values(by='time', inplace=True)
+    dataframe.drop(columns='weather', inplace=True, errors='ignore')
     dataframe.drop(index=dataframe.loc[start_time > dataframe['time'] > end_time].index, inplace=True, errors='ignore')
     dataframe['sensorId'] = sensor['sensorId']
 
