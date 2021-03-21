10d5d0ee041b305ee12bf4dbb2ea88be501e5a45
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-03-21 16:35:09 +0100

    Migrated the methods for fetching pollution data from the pulse.eco API to OpenWeather API, fixed a bug in the method for fetching weather data

--- src/preparation/pollution_data.py
+++ src/preparation/pollution_data.py
@@ -2,14 +2,10 @@ from datetime import datetime
 from os import environ, path
 from traceback import format_exc
 
-from numpy import int64
-from pandas import DataFrame, json_normalize, to_datetime as pandas_to_datetime, to_numeric
-from pytz import timezone
+from pandas import DataFrame
 from requests import get as requests_get
-from timezonefinder import TimezoneFinder
 
-from definitions import DATA_EXTERNAL_PATH, pulse_eco_password, pulse_eco_username, pollutants
-from processing.normalize_data import normalize_pollution_data
+from definitions import DATA_EXTERNAL_PATH, open_weather_token
 from .handle_data import save_dataframe
 
 week_in_seconds = 604800
@@ -24,62 +20,32 @@ def format_datetime(timestamp, tz):
 
 
 def fetch_pollution_data(city_name, sensor, start_time, end_time):
-    url = f'https://{city_name}.pulse.eco/rest/dataRaw'
-
-    username = environ[pulse_eco_username]
-    password = environ[pulse_eco_password]
-
-    tf = TimezoneFinder()
+    url = 'https://api.openweathermap.org/data/2.5/air_pollution/history'
     sensor_position = sensor['position'].split(',')
-    latitude, longitude = float(sensor_position[0]), float(sensor_position[1])
-    sensor_loc = tf.timezone_at(lng=longitude, lat=latitude)
-    sensor_tz = timezone(sensor_loc)
-
-    from_timestamp = start_time
-    from_datetime = format_datetime(from_timestamp, sensor_tz)
-
-    to_timestamp = start_time + week_in_seconds
-    to_datetime = format_datetime(to_timestamp, sensor_tz)
+    lat, lon = float(sensor_position[0]), float(sensor_position[1])
+    token = environ[open_weather_token]
+    params = f'lat={lat}&lon={lon}&start={start_time}&end={end_time}&appid={token}'
 
     dataframe = DataFrame()
-    while from_timestamp < end_time:
-        for pollutant in pollutants:
-            parameters = f'sensorId={sensor["sensorId"]}&type={pollutant}&from={from_datetime}&to={to_datetime}'
-            pollution_response = requests_get(url=url, params=parameters, auth=(username, password))
-            try:
-                pollution_json = pollution_response.json()
-                dataframe = dataframe.append(json_normalize(pollution_json), ignore_index=True)
-            except ValueError:
-                print(pollution_response)
-                print(format_exc())
-
-        if not dataframe.empty:
-            dataframe.sort_values(by='stamp', inplace=True)
-            dataframe['stamp'] = pandas_to_datetime(dataframe['stamp'])
-            last_datetime = dataframe['stamp'].iloc[-1]
-            last_timestamp = datetime.timestamp(last_datetime)
-            if from_timestamp < last_timestamp:
-                from_timestamp = last_timestamp
-                to_timestamp += week_in_seconds
-            else:
-                from_timestamp += week_in_seconds
-                to_timestamp += week_in_seconds
-            from_datetime = format_datetime(from_timestamp, sensor_tz)
-            to_datetime = format_datetime(to_timestamp, sensor_tz)
-        else:
-            from_timestamp += week_in_seconds
-            from_datetime = format_datetime(from_timestamp, sensor_tz)
-            to_timestamp += week_in_seconds
-            to_datetime = format_datetime(to_timestamp, sensor_tz)
-
-    dataframe.rename(columns={'stamp': 'time'}, inplace=True, errors='ignore')
-    dataframe['time'] = pandas_to_datetime(dataframe['time'], errors='ignore')
-    dataframe['time'] = dataframe['time'].values.astype(int64) // 10 ** 9
-    dataframe.drop(index=dataframe.loc[start_time > dataframe['time'] > end_time].index, inplace=True, errors='ignore')
-    dataframe['sensorId'] = sensor['sensorId']
-    dataframe['value'] = to_numeric(dataframe['value'], errors='ignore')
+    pollution_response = requests_get(url=url, params=params)
+    try:
+        pollution_json = pollution_response.json()
+        pollution_data = pollution_json['list']
+        data = []
+        for pollution in pollution_data:
+            pollution_dict = {'time': pollution['dt']}
+            pollution_dict.update(pollution['main'])
+            pollution_dict.update(pollution['components'])
+            data.append(pollution_dict)
+        dataframe = dataframe.append(DataFrame(data), ignore_index=True)
+    except ValueError:
+        print(pollution_response)
+        print(format_exc())
 
     if not dataframe.empty:
-        dataframe = normalize_pollution_data(dataframe)
+        dataframe.sort_values(by='time', inplace=True)
+        dataframe.drop(index=dataframe.loc[start_time > dataframe['time'] > end_time].index, inplace=True,
+                       errors='ignore')
+        dataframe['sensorId'] = sensor['sensorId']
         pollution_data_path = path.join(DATA_EXTERNAL_PATH, city_name, sensor['sensorId'], 'pollution.csv')
         save_dataframe(dataframe, 'pollution', pollution_data_path, sensor['sensorId'])
--- src/preparation/weather_data.py
+++ src/preparation/weather_data.py
@@ -21,23 +21,22 @@ def fetch_weather_data(city_name, sensor, start_time, end_time):
     params = f'lat={lat}&lon={lon}&units={units}&exclude={exclude}&appid={token}'
 
     dataframe = DataFrame()
-    while start_time < end_time:
-        weather_response = requests_get(url=url, params=params)
-        try:
-            weather_json = weather_response.json()
-            hourly_data = weather_json['hourly']
-            df = json_normalize([flatten_json(hourly) for hourly in hourly_data])
-            df.rename(columns={'dt': 'time'}, inplace=True, errors='ignore')
-            dataframe = dataframe.append(df, ignore_index=True)
-        except (KeyError, ValueError):
-            print(weather_response)
-            print(format_exc())
-
-    dataframe.sort_values(by='time', inplace=True)
-    dataframe.drop(columns='weather', inplace=True, errors='ignore')
-    dataframe.drop(index=dataframe.loc[start_time > dataframe['time'] > end_time].index, inplace=True, errors='ignore')
-    dataframe['sensorId'] = sensor['sensorId']
+    weather_response = requests_get(url=url, params=params)
+    try:
+        weather_json = weather_response.json()
+        hourly_data = weather_json['hourly']
+        df = json_normalize([flatten_json(hourly) for hourly in hourly_data])
+        df.rename(columns={'dt': 'time'}, inplace=True, errors='ignore')
+        dataframe = dataframe.append(df, ignore_index=True)
+    except (KeyError, ValueError):
+        print(weather_response)
+        print(format_exc())
 
     if not dataframe.empty:
+        dataframe.sort_values(by='time', inplace=True)
+        dataframe.drop(columns='weather', inplace=True, errors='ignore')
+        dataframe.drop(index=dataframe.loc[start_time > dataframe['time'] > end_time].index, inplace=True,
+                       errors='ignore')
+        dataframe['sensorId'] = sensor['sensorId']
         weather_data_path = path.join(DATA_EXTERNAL_PATH, city_name, sensor['sensorId'], 'weather.csv')
         save_dataframe(dataframe, 'weather', weather_data_path, sensor['sensorId'])
