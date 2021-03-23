0ec115e167338e198e4720c74e06f68fcc86a509
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-03-23 19:33:08 +0100

    Added list of countries to include for predicting air pollution, added directory for importing data manually with csv files, fixed warnings when merging data from weather and pollution, added scheduled method for importing data

--- definitions.py
+++ definitions.py
@@ -4,6 +4,7 @@ ROOT_DIR = path.dirname(path.abspath(__file__))
 
 DATA_PATH = path.join(ROOT_DIR, 'data')
 DATA_EXTERNAL_PATH = path.join(DATA_PATH, 'external')
+DATA_IMPORT_PATH = path.join(DATA_PATH, 'import')
 DATA_INTERIM_PATH = path.join(DATA_PATH, 'interim')
 DATA_PROCESSED_PATH = path.join(DATA_PATH, 'processed')
 DATA_RAW_PATH = path.join(DATA_PATH, 'raw')
@@ -41,6 +42,10 @@ environment_variables = [
 
 application_development = 'development'
 application_prod = 'prod'
+
+countries = {
+    'MK': 'Macedonia'
+}
 
 pollutants = {
     'co': 'CO',
