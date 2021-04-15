1847a0bfff6b24b44fbec5867794b2732a5390db
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-04-15 15:19:06 +0200

    Removed unused methods for normalizing data, reformatted code and fixed removing NaN values from dataframes

--- src/preparation/weather_data.py
+++ src/preparation/weather_data.py
@@ -1,24 +1,16 @@
-from datetime import datetime
 from os import environ, path
 from time import sleep
 from traceback import format_exc
 
-from pandas import DataFrame, json_normalize, read_csv
+from pandas import DataFrame, json_normalize
 from requests import get as requests_get
 
 from definitions import DATA_EXTERNAL_PATH, open_weather_token
-from processing import current_hour, flatten_json
+from processing import flatten_json
 from .handle_data import save_dataframe
 
 
 def fetch_weather_data(city_name, sensor):
-    current_datetime = current_hour(datetime.now())
-    start_time = int(datetime.timestamp(current_datetime))
-    dataframe = read_csv(path.join(DATA_EXTERNAL_PATH, city_name, sensor['sensorId'], 'weather.csv'))
-    last_timestamp = dataframe['time'].max()
-    if last_timestamp >= start_time:
-        return
-
     url = 'https://api.openweathermap.org/data/2.5/onecall'
     sensor_position = sensor['position'].split(',')
     lat, lon = float(sensor_position[0]), float(sensor_position[1])
