9c742895eeabd34d974de75e8eee28e7884d3ffb
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-04-15 16:33:35 +0200

    Optimized the forecast endpoint to retrieve predictions faster, refactored the methods for retrieving pollution forecasts for each upcoming hour in a 48 hour span

--- src/processing/forecast_data.py
+++ src/processing/forecast_data.py
@@ -1,15 +1,30 @@
+from datetime import datetime
+from math import nan
 from os import path
 
 from pandas import concat as pandas_concat, DataFrame, date_range, read_csv, Series, Timedelta
 
 from api.config.cache import cache
 from definitions import DATA_EXTERNAL_PATH
-from .feature_generation import encode_categorical_data, generate_features, generate_lag_features, \
+from .feature_generation import encode_categorical_data, generate_lag_features, \
     generate_time_features
 from .feature_scaling import value_scaling
+from .normalize_data import next_hour
 
 FORECAST_PERIOD = '1H'
-FORECAST_STEPS = 1
+FORECAST_STEPS = 48
+
+
+@cache.memoize(timeout=3600)
+def fetch_weather_features(city_name, sensor_id, model_features, timestamp):
+    forecast_data = forecast_sensor(city_name, sensor_id, timestamp)
+    data = {}
+    for model_feature in model_features:
+        feature_value = forecast_data.get(model_feature)
+        if feature_value is not None:
+            data[model_feature] = feature_value
+
+    return data
 
 
 @cache.memoize(timeout=3600)
@@ -18,10 +33,11 @@ def forecast_sensor(city_name, sensor_id, timestamp):
     dataframe = dataframe.loc[dataframe['time'] == timestamp]
     if not dataframe.empty:
         return dataframe.to_dict('records')[0]
+
     return {}
 
 
-def direct_forecast(y, model, lags=FORECAST_STEPS, n_steps=FORECAST_STEPS, step=FORECAST_PERIOD) -> Series:
+def direct_forecast(y, model, lags=FORECAST_STEPS, n_steps=FORECAST_STEPS, step=FORECAST_PERIOD):
     """Multi-step direct forecasting using a machine learning model to forecast each time period ahead
 
     Parameters
@@ -67,16 +83,18 @@ def direct_forecast(y, model, lags=FORECAST_STEPS, n_steps=FORECAST_STEPS, step=
     return Series(forecast_values, forecast_range)
 
 
-def recursive_forecast(y, city_name, sensor_id, model, model_features, n_steps=FORECAST_STEPS, step=FORECAST_PERIOD):
+def recursive_forecast(y, city_name, sensor_id, model, model_features, lags=FORECAST_STEPS, n_steps=FORECAST_STEPS,
+                       step=FORECAST_PERIOD):
     """Multi-step recursive forecasting using the input time series data and a pre-trained machine learning model
 
     Parameters
     ----------
     y: pd.Series holding the input time-series to forecast
     city_name: The name of the city where the sensor is located
-    sensor_id: The ID of the sensor to forecast weather data
+    sensor_id: The ID of the sensor to fetch weather data
     model: An already trained machine learning model implementing the scikit-learn interface
     model_features: Selected model features for forecasting
+    lags: List of lags used for training the model
     n_steps: Number of time periods in the forecasting horizon
     step: The period of forecasting
 
@@ -85,9 +103,9 @@ def recursive_forecast(y, city_name, sensor_id, model, model_features, n_steps=F
     forecast_values: pd.Series with forecasted values indexed by forecast horizon dates
     """
 
-    # Get the dates to forecast
-    last_date = y.index[-1] + Timedelta(hours=1)
-    forecast_range = date_range(last_date, periods=n_steps, freq=step)
+    # Get the hours to forecast
+    upcoming_hour = next_hour(datetime.now())
+    forecast_range = date_range(upcoming_hour, periods=n_steps, freq=step)
 
     forecasted_values = []
     target = y.copy()
@@ -95,29 +113,31 @@ def recursive_forecast(y, city_name, sensor_id, model, model_features, n_steps=F
     for date in forecast_range:
         # Build target time series using previously forecast value
         new_point = forecasted_values[-1] if len(forecasted_values) > 0 else 0.0
-        target = target.append(Series(index=[date], data=new_point))
+        target = target.append(Series(new_point, [date]))
 
         # Forecast
         timestamp = int((date - Timedelta(hours=1)).timestamp())
-        forecast_data = forecast_sensor(city_name, sensor_id, timestamp)
-        data = {}
-        for model_feature in model_features:
-            feature_value = forecast_data.get(model_feature)
-            if feature_value is not None:
-                data[model_feature] = feature_value
-
+        data = fetch_weather_features(city_name, sensor_id, model_features, timestamp)
         if not data:
-            return None
+            forecasted_values.append(nan)
+            target.update(Series(forecasted_values[-1], [target.index[-1]]))
+            continue
 
         dataframe = DataFrame(data, index=[date])
-        dataframe = dataframe.join(generate_features(target), how='inner').dropna()
-        dataframe = pandas_concat(
-            [dataframe, DataFrame(columns=list(set(model_features) - set(list(dataframe.columns))))])
-        encode_categorical_data(dataframe)
-        dataframe = dataframe[model_features]
-        dataframe = value_scaling(dataframe)
-        predictions = model.predict(dataframe)
+        lags_features = generate_lag_features(target, lags)
+        time_features = generate_time_features(target)
+        features = dataframe.join(lags_features, how='inner').join(time_features, how='inner')
+        features = pandas_concat([features, DataFrame(columns=list(set(model_features) - set(list(features.columns))))])
+        encode_categorical_data(features)
+        features = features[model_features]
+        try:
+            features = value_scaling(features)
+            predictions = model.predict(features)
+        except ValueError:
+            forecasted_values.append(nan)
+            target.update(Series(forecasted_values[-1], [target.index[-1]]))
+            continue
         forecasted_values.append(predictions[-1])
-        target.update(Series(forecasted_values[-1], index=[target.index[-1]]))
+        target.update(Series(forecasted_values[-1], [target.index[-1]]))
 
-    return Series(forecasted_values, forecast_range).iloc[-1]
+    return Series(forecasted_values, forecast_range)
