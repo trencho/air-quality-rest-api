9611b56227a6337e8d2169bc6ca8819fb52c0e80
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-06-11 18:46:15 +0200

    Added custom configuration for initializing swagger

--- src/api/config/swagger.py
+++ src/api/config/swagger.py
@@ -1,10 +1,18 @@
 from flasgger import Swagger
 
-swagger = Swagger()
+config = {
+    'openapi': '3.0.3',
+    'specs': [
+        {
+            'endpoint': 'apispec_1',
+            'route': '/api/v1/apispec_1.json',
+            'rule_filter': lambda rule: True,  # all in
+            'model_filter': lambda tag: True,  # all in
+        }
+    ],
+    'specs_route': '/api/v1/apidocs/'}
+swagger = Swagger(config=config, merge=True)
 
 
 def configure_swagger(app):
-    app.config['SWAGGER'] = {
-        'openapi': '3.0.3'
-    }
     swagger.init_app(app)
