0c500d621ed77bb4d3b4ccaf8cf9f440643e73b6
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-08-15 10:29:07 +0200

    Modified the name of variables used in the data dump git file

--- src/api/config/git.py
+++ src/api/config/git.py
@@ -18,10 +18,10 @@ def append_commit_files(file_list, file_names, root, data, file):
     file_names.append(rel_file)
 
 
-def merge_csv_files(repository_name, file_name, data):
+def merge_csv_files(repo_name, file_name, data):
     local_file_content = read_csv(StringIO(data))
     try:
-        repo = g.get_user().get_repo(repository_name)
+        repo = g.get_user().get_repo(repo_name)
         repo_file = repo.get_contents(file_name)
         repo_file_content = read_csv(BytesIO(repo_file.decoded_content))
         local_file_content = local_file_content.append(repo_file_content, ignore_index=True, sort=True)
@@ -46,16 +46,16 @@ def commit_git_files(repo, element_list, base_tree, master_sha, commit_message, 
         print_exc()
 
 
-def update_git_files(file_names, file_list, repository_name, branch,
+def update_git_files(file_names, file_list, repo_name, branch,
                      commit_message=f'Data Updated - {datetime.now().strftime("%H:%M:%S %d-%m-%Y")}'):
-    repo = g.get_user().get_repo(repository_name)
+    repo = g.get_user().get_repo(repo_name)
     master_ref = repo.get_git_ref(f'heads/{branch}')
     master_sha = master_ref.object.sha
     base_tree = repo.get_git_tree(master_sha)
     element_list = []
     for i in range(0, len(file_list)):
         if file_names[i].endswith('.csv'):
-            file_list[i] = merge_csv_files(repository_name, file_names[i], file_list[i])
+            file_list[i] = merge_csv_files(repo_name, file_names[i], file_list[i])
             element = InputGitTreeElement(file_names[i], '100644', 'blob', file_list[i])
             element_list.append(element)
         elif file_names[i].endswith('.png'):
