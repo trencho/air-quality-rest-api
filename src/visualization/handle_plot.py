fd1ae0effc12e5702b541701c3eb7dfe4fe1ff42
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-09-30 14:10:50 +0200

    Removed datetime info in plot name

--- src/visualization/handle_plot.py
+++ src/visualization/handle_plot.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from os import makedirs, path
 
 from matplotlib import pyplot
@@ -9,6 +8,5 @@ def save_plot(fig: Figure, plt: pyplot, file_path: str, file_name: str) -> None:
     fig.tight_layout()
 
     makedirs(file_path, exist_ok=True)
-    plt.savefig(path.join(file_path, f'{file_name} - {datetime.now().strftime("%H-%M-%S %d-%m-%Y")}.png'),
-                bbox_inches='tight')
+    plt.savefig(path.join(file_path, f'{file_name}.png'), bbox_inches='tight')
     plt.close(fig)
