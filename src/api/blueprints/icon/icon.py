0996f5486d26012e842f18d75fd2629541c2a693
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-10-03 13:05:27 +0200

    Set the cache of the favicon to never expire

--- src/api/blueprints/icon/icon.py
+++ src/api/blueprints/icon/icon.py
@@ -2,9 +2,12 @@ from os import path
 
 from flask import Blueprint, send_from_directory
 
+from api.config.cache import cache
+
 icon_blueprint = Blueprint('icon', __name__)
 
 
 @icon_blueprint.route('/favicon.ico/', methods=['GET'])
+@cache.cached(timeout=0)
 def favicon():
     return send_from_directory(path.join(icon_blueprint.root_path, 'icon'), 'aqra-logo.svg')
