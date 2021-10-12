316c1a299f3d5555bc4e7f511e7fc562ac2c685e
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-10-12 21:30:11 +0200

    Altered the criterion parameter values for the decision tree regression model, fixed the list comprehension when appending list of values, removed the deprecated normalize parameter for the linear regression model

--- src/api/blueprints/forecast/forecast.py
+++ src/api/blueprints/forecast/forecast.py
@@ -55,9 +55,9 @@ def return_forecast_results(latitude: float, longitude: float, city: dict, senso
         return make_response(forecast_results)
 
     forecast_result = fetch_forecast_result(city, sensor, daemon=True)
-    forecast_results['data'] = [forecast_result.values()]
+    forecast_results['data'] = [*forecast_result.values()]
     if mongodb_env is not None:
         mongo.db['predictions'].replace_one({'cityName': city['cityName'], 'sensorId': sensor['sensorId']},
-                                            {'data': [forecast_result.values()],
+                                            {'data': [*forecast_result.values()],
                                              'cityName': city['cityName'], 'sensorId': sensor['sensorId']}, upsert=True)
     return make_response(forecast_results)
