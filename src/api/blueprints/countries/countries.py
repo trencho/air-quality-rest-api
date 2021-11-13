86c6d33cd4113f519dda4c40d22039c28cb7166f
Aleksandar Trenchevski <atrenchevski@gmail.com>
2021-11-13 15:58:01 +0100

    Fixed missing endpoint for countries

--- src/api/blueprints/countries/countries.py
+++ src/api/blueprints/countries/countries.py
@@ -9,7 +9,7 @@ countries_blueprint = Blueprint('countries', __name__)
 
 
 @countries_blueprint.route('/countries/', endpoint='countries', methods=['GET'])
-@countries_blueprint.route('/countries/<string:country_code>/', endpoint='country_code', methods=['GET'])
+@countries_blueprint.route('/countries/<string:country_code>/', endpoint='countries_code', methods=['GET'])
 @cache.memoize(timeout=3600)
 @swag_from('countries.yml', endpoint='countries.countries', methods=['GET'])
 @swag_from('countries_code.yml', endpoint='countries.countries_code', methods=['GET'])
