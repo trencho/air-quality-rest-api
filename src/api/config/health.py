083e37140c642c51207134403cc825e5d1d7fe22
Aleksandar Trenchevski <atrenchevski@gmail.com>
2022-02-28 17:57:48 +0100

    Removed expiration time for the health check response

--- src/api/config/health.py
+++ src/api/config/health.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, make_response, Response
 from flask_healthz import Healthz
 from starlette.status import HTTP_200_OK
 
+from .cache import cache
+
 HEALTHZ = {
     'live': 'api.config.health.liveness'
 }
@@ -12,5 +14,6 @@ def configure_healthcheck(app: Flask) -> None:
     Healthz(app, no_log=True)
 
 
+@cache.cached(timeout=0)
 def liveness() -> Response:
     return make_response(jsonify(error_message='OK'), HTTP_200_OK)
