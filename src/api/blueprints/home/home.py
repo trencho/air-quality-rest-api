d72623330660e5943e8ce12214d88421e0d0eaf6
Aleksandar Trenchevski <atrenchevski@gmail.com>
2022-04-22 01:31:56 +0200

    Changed the flasgger static file url path, removed redundant home blueprint

--- src/api/blueprints/home/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, Response
-
-home_blueprint = Blueprint('home', __name__)
-
-
-@home_blueprint.route('/', methods=['GET'])
-def home() -> Response:
-    return redirect('https://aqra.feit.ukim.edu.mk/ui')
--- src/api/config/swagger.py
+++ src/api/config/swagger.py
@@ -1,21 +1,21 @@
 from flasgger import Swagger
 from flask import Flask
 
-config = {
-    'favicon': '/favicon.ico/',
-    'openapi': '3.0.3',
-    'specs': [
-        {
-            'endpoint': 'apispec_1',
-            'route': '/api/v1/apispec_1.json',
-            'rule_filter': lambda rule: True,
-            'model_filter': lambda tag: True,
-        }
-    ],
-    'specs_route': '/api/v1/apidocs/',
-    'title': 'AQRA'
-}
-swagger = Swagger(config=config, merge=True)
+config = Swagger.DEFAULT_CONFIG
+config['favicon'] = '/api/v1/favicon.ico/'
+config['openapi'] = '3.0.3'
+config['specs'] = [
+    {
+        'endpoint': 'apispec_1',
+        'route': '/api/v1/apispec_1.json',
+        'rule_filter': lambda rule: True,
+        'model_filter': lambda tag: True,
+    }
+]
+config['static_url_path'] = '/api/v1/flasgger_static'
+config['specs_route'] = '/api/v1/apidocs/'
+config['title'] = 'AQRA'
+swagger = Swagger(config=config)
 
 
 def configure_swagger(app: Flask) -> None:
