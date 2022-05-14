523b9ac9ef2f0a9bb6dab879725a381e79e6a31a
Aleksandar Trenchevski <atrenchevski@gmail.com>
2022-05-14 21:12:50 +0200

    Removed method for calculating forecast result during the forecast response execution

--- src/api/blueprints/forecast/forecast.py
+++ src/api/blueprints/forecast/forecast.py
@@ -8,7 +8,6 @@ from api.config.cache import cache
 from api.config.database import mongo
 from definitions import mongodb_connection
 from preparation import calculate_nearest_sensor, check_city, check_sensor, read_cities
-from processing import fetch_forecast_result
 
 forecast_blueprint = Blueprint('forecast', __name__)
 
@@ -46,18 +45,12 @@ def fetch_coordinates_forecast(latitude: float, longitude: float) -> Response:
 
 
 def return_forecast_results(latitude: float, longitude: float, city: dict, sensor: dict) -> Response:
-    forecast_results = {'latitude': latitude, 'longitude': longitude}
-    if (mongodb_env := environ.get(mongodb_connection)) is not None:
+    forecast_results = {'latitude': latitude, 'longitude': longitude, 'data': []}
+    if environ.get(mongodb_connection) is not None:
         forecast_result = mongo.db['predictions'].find_one(
             {'cityName': city['cityName'], 'sensorId': sensor['sensorId']})
         if forecast_result is not None and forecast_result['data']:
             forecast_results['data'] = forecast_result['data']
             return make_response(forecast_results)
 
-    forecast_result = fetch_forecast_result(city, sensor)
-    forecast_results['data'] = list(forecast_result.values())
-    if mongodb_env is not None:
-        mongo.db['predictions'].replace_one({'cityName': city['cityName'], 'sensorId': sensor['sensorId']},
-                                            {'data': list(forecast_result.values()),
-                                             'cityName': city['cityName'], 'sensorId': sensor['sensorId']}, upsert=True)
     return make_response(forecast_results)
