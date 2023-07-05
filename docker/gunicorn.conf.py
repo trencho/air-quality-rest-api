99bb0bddc883e66991c5b068617fc2cab11a8c57
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-05 10:32:29 +0200

    Removed unused user id and group

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -1,8 +1,6 @@
 from multiprocessing import cpu_count
-from os import environ
 
 from api.config.schedule import scheduler
-from definitions import USER_GID, USER_ID
 
 wsgi_app = "app:app"
 disable_redirect_access_to_syslog = True
@@ -15,8 +13,6 @@ def on_starting(server):
 preload_app = True
 chdir = "/app/src/api"
 pidfile = "/run/.pid"
-uid = environ[USER_ID]
-gid = environ[USER_GID]
 bind = ["unix:/run/gunicorn.socket"]
 workers = cpu_count() * 2 + 1
 threads = 2
