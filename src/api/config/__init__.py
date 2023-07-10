e3deaabddca36455276438adf4f2f1ed6d81c7e8
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-11 01:20:26 +0200

    Removed rate limiter, enabled CORS in ingress, removed CORS headers from lower level setup

--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask
 from definitions import APP_DEV, APP_ENV, APP_PROD
 from .blueprints import register_blueprints
 from .cache import configure_cache
-from .cors import configure_cors
 from .environment import check_environment_variables, fetch_data, init_system_paths
 from .health import configure_healthcheck
 from .logger import configure_logger
@@ -25,7 +24,6 @@ def create_app() -> Flask:
     register_blueprints(app)
 
     configure_cache(app)
-    configure_cors(app)
     configure_healthcheck(app)
     configure_logger()
     configure_swagger(app)
--- src/api/config/cors.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_cors import CORS
-
-from definitions import URL_PREFIX
-
-
-def configure_cors(app: Flask) -> None:
-    CORS(app, resources=rf"{URL_PREFIX}/*")
