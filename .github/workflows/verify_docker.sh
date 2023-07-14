80e4c5916afb85b06e9f48337caca364c9566868
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-14 16:48:36 +0200

    Moved the hash files to an upper level

--- .github/workflows/verify_docker.sh
+++ .github/workflows/verify_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="${GIT_REPO_PATH}"
-EXCLUDE_KUBERNETES_DIR="kubernetes"
+EXCLUDE_KUBERNETES_DIR="${SOURCE_DIR}/kubernetes"
 SOURCE_DIR_SHA_FILE="source_dir_sha"
 current_sha=$(hash_directory "${SOURCE_DIR}" "${EXCLUDE_KUBERNETES_DIR}")
 if [ -f "${SOURCE_DIR_SHA_FILE}" ]; then
