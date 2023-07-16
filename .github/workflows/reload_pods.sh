9598b3ea4f28f24e15327d1c489774dcdcb6c4a5
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-16 16:57:38 +0200

    Refactored the deploy-build.yml with custom shell scripts, added a function for hashing directories, updated common.txt (lightgbm 3.3.5 -> 4.0.0), updated dev.txt (dnspython 2.3.0 -> 2.4.0)

---
+++ .github/workflows/reload_pods.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl get pods -n aqra | grep -E 'mongo[a-z0-9\-]*' -iwo | tr -d '\n' | xargs kubectl delete pod -n aqra
+kubectl get pods -n aqra | grep -E 'flask[a-z0-9\-]*' -iwo | tr -d '\n' | xargs kubectl delete pod -n aqra
+echo Sleeping for 30 seconds for the deployments to pull the new images
+sleep 30
+docker system prune -a --volumes --force
--- .github/workflows/verify_kubernetes.sh
+++ .github/workflows/verify_kubernetes.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-KUBERNETES_DIR="kubernetes"
+source "${GIT_REPO_PATH}"/.github/workflows//hash_directory.sh
+
+KUBERNETES_DIR="${GIT_REPO_PATH}/kubernetes"
 KUBERNETES_DIR_SHA_FILE="kubernetes_dir_sha"
 current_sha=$(hash_directory "${KUBERNETES_DIR}")
 if [ -f "${KUBERNETES_DIR_SHA_FILE}" ]; then
   stored_sha=$(cat "${KUBERNETES_DIR_SHA_FILE}")
   if [ "${stored_sha}" = "${current_sha}" ]; then
-    echo "SHA values match: The Kubernetes directory has not changed."
+    echo "SHA values match: The kubernetes directory has not changed."
   else
     echo "SHA values differ: The Kubernetes directory has been modified."
     kubectl apply -f "${KUBERNETES_DIR}"/resources.yml
@@ -14,6 +16,6 @@ if [ -f "${KUBERNETES_DIR_SHA_FILE}" ]; then
 else
   current_sha=$(hash_directory "${KUBERNETES_DIR}")
   echo "File '${KUBERNETES_DIR_SHA_FILE}' created with the current SHA value."
-  kubectl apply -f kubernetes/resources.yml
+  kubectl apply -f "${KUBERNETES_DIR}"/resources.yml
 fi
 echo "${current_sha}" >"${KUBERNETES_DIR_SHA_FILE}"
