5463ed07fb5a73de24495f98648afe93799c908e
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-17 12:26:26 +0200

    Fixed the deploy-build.yml to store the sha values outside the source directories, expanded the docker commands to execute without a prompt, fixed the GitHub instance and properly fetch the repository, added return type for the database repository method

--- .github/workflows/verify_kubernetes.sh
+++ .github/workflows/verify_kubernetes.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-source "${GIT_REPO_PATH}"/.github/workflows//hash_directory.sh
+source "${GIT_REPO_PATH}"/.github/workflows/hash_directory.sh
 
 KUBERNETES_DIR="${GIT_REPO_PATH}/kubernetes"
-KUBERNETES_DIR_SHA_FILE="kubernetes_dir_sha"
+FLASK_KUBERNETES_DIR_SHA_FILE="flask_kubernetes_dir_sha"
 current_sha=$(hash_directory "${KUBERNETES_DIR}")
-if [ -f "${KUBERNETES_DIR_SHA_FILE}" ]; then
-  stored_sha=$(cat "${KUBERNETES_DIR_SHA_FILE}")
+if [ -f "${FLASK_KUBERNETES_DIR_SHA_FILE}" ]; then
+  stored_sha=$(cat "${FLASK_KUBERNETES_DIR_SHA_FILE}")
   if [ "${stored_sha}" = "${current_sha}" ]; then
     echo "SHA values match: The kubernetes directory has not changed."
   else
     echo "SHA values differ: The Kubernetes directory has been modified."
     kubectl apply -f "${KUBERNETES_DIR}"/resources.yml
+    kubectl apply -f "${KUBERNETES_DIR}"/mongo-deployment.yml
+    kubectl apply -f "${KUBERNETES_DIR}"/single/resources/flask-deployment.yml
   fi
 else
   current_sha=$(hash_directory "${KUBERNETES_DIR}")
-  echo "File '${KUBERNETES_DIR_SHA_FILE}' created with the current SHA value."
+  echo "File '${FLASK_KUBERNETES_DIR_SHA_FILE}' created with the current SHA value."
   kubectl apply -f "${KUBERNETES_DIR}"/resources.yml
+  kubectl apply -f "${KUBERNETES_DIR}"/mongo-deployment.yml
+  kubectl apply -f "${KUBERNETES_DIR}"/single/resources/flask-deployment.yml
 fi
-echo "${current_sha}" >"${KUBERNETES_DIR_SHA_FILE}"
+echo "${current_sha}" >"${FLASK_KUBERNETES_DIR_SHA_FILE}"
