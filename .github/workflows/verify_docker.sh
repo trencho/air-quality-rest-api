60e3076535361111ba0fd7a8bf7230fd78abba24
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-17 14:38:02 +0200

    Renamed the sha file names for the source and kubernetes directory

--- .github/workflows/verify_docker.sh
+++ .github/workflows/verify_docker.sh
@@ -4,10 +4,10 @@ source "${GIT_REPO_PATH}"/.github/workflows/hash_directory.sh
 
 SOURCE_DIR="${GIT_REPO_PATH}"
 EXCLUDE_KUBERNETES_DIR="kubernetes"
-FLASK_SOURCE_DIR_SHA_FILE="flask_source_dir_sha"
+SOURCE_DIR_SHA_FILE="${GIT_REPO_NAME}_source_dir_sha"
 current_sha=$(hash_directory "${SOURCE_DIR}" "${EXCLUDE_KUBERNETES_DIR}")
-if [ -f "${FLASK_SOURCE_DIR_SHA_FILE}" ]; then
-  stored_sha=$(cat "${FLASK_SOURCE_DIR_SHA_FILE}")
+if [ -f "${SOURCE_DIR_SHA_FILE}" ]; then
+  stored_sha=$(cat "${SOURCE_DIR_SHA_FILE}")
   if [ "${stored_sha}" = "${current_sha}" ]; then
     echo "SHA values match: The source directory has not changed."
   else
@@ -16,7 +16,7 @@ if [ -f "${FLASK_SOURCE_DIR_SHA_FILE}" ]; then
   fi
 else
   current_sha=$(hash_directory "${SOURCE_DIR}" "${EXCLUDE_KUBERNETES_DIR}")
-  echo "File '${FLASK_SOURCE_DIR_SHA_FILE}' created with the current SHA value."
+  echo "File '${SOURCE_DIR_SHA_FILE}' created with the current SHA value."
   docker-compose -f "${SOURCE_DIR}"/docker/single/docker-compose.yml build
 fi
-echo "${current_sha}" >"${FLASK_SOURCE_DIR_SHA_FILE}"
+echo "${current_sha}" >"${SOURCE_DIR_SHA_FILE}"
