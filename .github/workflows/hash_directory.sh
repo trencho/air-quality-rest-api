2b0727871fb3d7bce410987939b861eb6ac155a8
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-07-17 14:38:02 +0200

    Renamed the sha file names for the source and kubernetes directory

--- .github/workflows/hash_directory.sh
+++ .github/workflows/hash_directory.sh
@@ -6,12 +6,10 @@ hash_directory() {
   hash=""
 
   while IFS= read -r -d '' file; do
-    # Exclude the specified directory if provided
     if [[ -n "$exclude_directory" && "$file" == *"$exclude_directory"* ]]; then
       continue
     fi
 
-    # Calculate hash of each file
     file_hash=$(sha256sum "$file" | awk '{print $1}')
     hash="$hash$file_hash"
   done < <(find "$directory" -type f -print0 | LC_ALL=C sort -z)
--- .github/workflows/verify_kubernetes.sh
+++ .github/workflows/verify_kubernetes.sh
@@ -3,10 +3,10 @@
 source "${GIT_REPO_PATH}"/.github/workflows/hash_directory.sh
 
 KUBERNETES_DIR="${GIT_REPO_PATH}/kubernetes"
-FLASK_KUBERNETES_DIR_SHA_FILE="flask_kubernetes_dir_sha"
+KUBERNETES_DIR_SHA_FILE="${GIT_REPO_NAME}_kubernetes_dir_sha"
 current_sha=$(hash_directory "${KUBERNETES_DIR}")
-if [ -f "${FLASK_KUBERNETES_DIR_SHA_FILE}" ]; then
-  stored_sha=$(cat "${FLASK_KUBERNETES_DIR_SHA_FILE}")
+if [ -f "${KUBERNETES_DIR_SHA_FILE}" ]; then
+  stored_sha=$(cat "${KUBERNETES_DIR_SHA_FILE}")
   if [ "${stored_sha}" = "${current_sha}" ]; then
     echo "SHA values match: The kubernetes directory has not changed."
   else
@@ -17,9 +17,9 @@ if [ -f "${FLASK_KUBERNETES_DIR_SHA_FILE}" ]; then
   fi
 else
   current_sha=$(hash_directory "${KUBERNETES_DIR}")
-  echo "File '${FLASK_KUBERNETES_DIR_SHA_FILE}' created with the current SHA value."
+  echo "File '${KUBERNETES_DIR_SHA_FILE}' created with the current SHA value."
   kubectl apply -f "${KUBERNETES_DIR}"/resources.yml
   kubectl apply -f "${KUBERNETES_DIR}"/mongo-deployment.yml
   kubectl apply -f "${KUBERNETES_DIR}"/single/resources/flask-deployment.yml
 fi
-echo "${current_sha}" >"${FLASK_KUBERNETES_DIR_SHA_FILE}"
+echo "${current_sha}" >"${KUBERNETES_DIR_SHA_FILE}"
