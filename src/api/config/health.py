dbbd67bbf201b2db6e8bc0ccfe5bf62bf639b250
Aleksandar Trenchevski <atrenchevski@gmail.com>
2023-08-18 12:41:00 +0200

    Updated common.txt (scipy 1.11.1 -> 1.11.2), modified the health endpoint prefix, added checks for status codes from the weather/pollution response

--- src/api/config/health.py
+++ src/api/config/health.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, make_response, Response
 from flask_healthz import Healthz
 from starlette.status import HTTP_200_OK
 
+from definitions import URL_PREFIX
 from .cache import cache
 
 
 def configure_healthcheck(app: Flask) -> None:
     app.config["HEALTHZ"] = {"live": "api.config.health.liveness"}
-    Healthz(app, no_log=True)
+    Healthz(app, prefix=f"{URL_PREFIX}/healthz", no_log=True)
 
 
 @cache.cached(timeout=0)
