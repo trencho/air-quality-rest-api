ea4df5af2680a2ed52f7d524be69bb8315fcd60d
Aleksandar Trenchevski <atrenchevski@gmail.com>
2024-01-10 14:59:13 +0100

    Added a new endpoint for returning plots for errors and predictions of the trained model for each pollutant, removed redundant command for changing directories in the deployment script, changed the name of the mongo client to be more verbose

--- src/api/config/__init__.py
+++ src/api/config/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from definitions import APP_DEV, APP_ENV, APP_PROD
 from .blueprints import register_blueprints
 from .cache import configure_cache
+from .converters import configure_converters
 from .cors import configure_cors
 from .environment import check_environment_variables, fetch_data, init_system_paths
 from .health import configure_healthcheck
@@ -21,6 +22,7 @@ def create_app() -> Flask:
     if environ.get(APP_ENV, APP_DEV) == APP_PROD:
         check_environment_variables()
 
+    configure_converters(app)
     register_blueprints(app)
 
     configure_cache(app)
