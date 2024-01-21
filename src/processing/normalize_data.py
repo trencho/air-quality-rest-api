b3051723e50c10d0d275291bad1a12667eb3ee2e
Aleksandar Trenchevski <atrenchevski@gmail.com>
2024-01-21 22:07:05 +0100

    Fixed NotImplementedError when applying parallelization

--- src/processing/normalize_data.py
+++ src/processing/normalize_data.py
@@ -42,7 +42,7 @@ def drop_numerical_outliers_with_iqr_score(dataframe: DataFrame, low: float = .0
     df = dataframe.loc[:, dataframe.columns != "time"]
     quant_df = df.quantile([low, high])
     df = df.apply(lambda x: x[(x > quant_df.loc[low, x.name]) & (x < quant_df.loc[high, x.name])], engine="numba",
-                  engine_kwargs={"parallel": True}, axis=0)
+                  engine_kwargs={"parallel": True, "raw": True}, axis=0)
     df = concat([dataframe.loc[:, "time"], df], axis=1)
     return df.dropna()
 
@@ -112,8 +112,8 @@ def process_data(city_name: str, sensor_id: str, collection: str) -> None:
 
         imp = KNNImputer()
         dataframe_raw[df_columns] = dataframe_raw[df_columns].apply(to_numeric, engine="numba",
-                                                                    engine_kwargs={"parallel": True}, axis="columns",
-                                                                    errors="coerce")
+                                                                    engine_kwargs={"parallel": True, "raw": True},
+                                                                    axis="columns", errors="coerce")
         for column in df_columns:
             if dataframe_raw[column].isna().all():
                 dataframe_raw.drop(columns=column, inplace=True, errors="ignore")
@@ -133,7 +133,8 @@ def process_data(city_name: str, sensor_id: str, collection: str) -> None:
 
         if collection != "weather":
             dataframe_raw["aqi"] = dataframe_raw[list(POLLUTANTS)].apply(calculate_index, engine="numba",
-                                                                         engine_kwargs={"parallel": True}, axis=1)
+                                                                         engine_kwargs={"parallel": True, "raw": True},
+                                                                         axis=1)
 
         # dataframe_raw = drop_numerical_outliers_with_z_score(dataframe_raw)
 
