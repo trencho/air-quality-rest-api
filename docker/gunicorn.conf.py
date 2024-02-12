81f922ac15f8a061a2ad2f0ae7a239c77fd603ab
Aleksandar Trenchevski <atrenchevski@gmail.com>
2024-02-12 19:55:38 +0100

    Removed preloading the application in order for worker processes to share the same memory

--- docker/gunicorn.conf.py
+++ docker/gunicorn.conf.py
@@ -3,7 +3,6 @@ from multiprocessing import cpu_count
 wsgi_app = "app:app"
 disable_redirect_access_to_syslog = True
 
-preload_app = True
 chdir = "/app/src/api"
 pidfile = "/run/.pid"
 bind = ["unix:/run/gunicorn.socket"]
