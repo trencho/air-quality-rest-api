f7f0913f565ec544d420247589dff53fd5c82188
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-01-04 17:35:05 +0100

    Enabled workflows

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+# Constants
+DEFAULT_TAG_NAME="prototype"
+DEFAULT_TAG_NUMBER="latest"
+
+# Check for required arguments
 if [ -z "$1" ]; then
   echo "You must specify a Dockerfile!"
   exit 1
 else
   dockerfile=$1
 fi
+
 if [ -z "$2" ]; then
   echo "You must specify a Docker Hub username!"
   exit 1
 else
   username=$2
 fi
-if [ -z "$3" ]; then
-  echo "Missing tag name, setting to \"prototype\""
-  tag_name=prototype
-else
-  tag_name=$3
-fi
-if [ -z "$4" ]; then
-  echo "Missing tag number, setting to \"latest\""
-  tag_number=latest
-else
-  tag_number=$4
-fi
 
-docker build -f "${dockerfile}" -t "${tag_name}":"${tag_number}" .
-docker tag "${tag_name}":"${tag_number}" "${username}"/"${tag_name}"
-docker push "${username}"/"${tag_name}"
+# Set optional arguments with default values
+tag_name=${3:-$DEFAULT_TAG_NAME}
+tag_number=${4:-$DEFAULT_TAG_NUMBER}
+
+# Build, tag, and push the Docker image
+docker build -f "${dockerfile}" -t "${tag_name}:${tag_number}" .
+docker tag "${tag_name}:${tag_number}" "${username}/${tag_name}"
+docker push "${username}/${tag_name}"
