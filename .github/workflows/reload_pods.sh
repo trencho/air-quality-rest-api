e7490300715e12c4d36e60f9c888eb02a312253d
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-03-07 23:03:50 +0100

    Removed checks for online status of the deployments

--- .github/workflows/reload_pods.sh
+++ .github/workflows/reload_pods.sh
@@ -12,13 +12,13 @@ kubectl rollout restart deployment mongo -n aqra
 #kubectl rollout status deployment/flask -n aqra --watch=true
 #kubectl rollout status deployment/mongo -n aqra --watch=true
 
-echo "Checking pod readiness..."
-for deployment in flask mongo; do
-  until [[ $(kubectl get pods -n aqra -l app=$deployment -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]]; do
-    echo "Waiting for $deployment pod to be ready..."
-    sleep 5
-  done
-done
+#echo "Checking pod readiness..."
+#for deployment in flask mongo; do
+#  until [[ $(kubectl get pods -n aqra -l app=$deployment -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]]; do
+#    echo "Waiting for $deployment pod to be ready..."
+#    sleep 5
+#  done
+#done
 
 echo "Cleaning up unused Docker resources..."
 docker system prune -a --volumes --force
