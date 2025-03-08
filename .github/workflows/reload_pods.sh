1ad9d04fc111ae079833092a914af6e7ecf5274f
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-03-08 14:00:18 +0100

    Removed redundant checks for deployments

--- .github/workflows/reload_pods.sh
+++ .github/workflows/reload_pods.sh
@@ -9,13 +9,5 @@ echo "Waiting for deployments to be ready..."
 kubectl rollout status deployment/flask -n aqra --watch=true
 kubectl rollout status deployment/mongo -n aqra --watch=true
 
-#echo "Checking pod readiness..."
-#for deployment in flask mongo; do
-#  until [[ $(kubectl get pods -n aqra -l app=$deployment -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') == "True" ]]; do
-#    echo "Waiting for $deployment pod to be ready..."
-#    sleep 5
-#  done
-#done
-
 echo "Cleaning up unused Docker resources..."
 docker system prune -a --volumes --force
