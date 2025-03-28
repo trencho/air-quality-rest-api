2678439a9731a7859343bea84b654a80ef0dadfd
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-03-28 12:58:27 +0100

    Add enum values for data types and error types in YAML configurations

--- src/visualization/algorithm_errors.py
+++ src/visualization/algorithm_errors.py
@@ -5,12 +5,12 @@ from pandas import DataFrame, read_csv
 from definitions import POLLUTANTS, REGRESSION_MODELS, RESULTS_ERRORS_PATH
 from .handle_plot import save_plot
 
-ERROR_TYPES = [
-    "Mean Absolute Error",
-    "Mean Absolute Percentage Error",
-    "Mean Squared Error",
-    "Root Mean Squared Error"
-]
+ERROR_TYPES = {
+    "mae": "Mean Absolute Error",
+    "mape": "Mean Absolute Percentage Error",
+    "mse": "Mean Squared Error",
+    "rmse": "Root Mean Squared Error"
+}
 
 PLOT_PARAMS = {
     "legend.fontsize": 16,
@@ -29,13 +29,13 @@ def draw_errors(city: dict, sensor: dict, pollutant: str) -> None:
     pyplot.style.use("seaborn-v0_8-whitegrid")
     seaborn.set_style("white")
 
-    for error_type in ERROR_TYPES:
+    for error_key, error_val in ERROR_TYPES.items():
         data = []
         for model_name in REGRESSION_MODELS:
             dataframe_errors = read_csv(
                 RESULTS_ERRORS_PATH / "data" / city["cityName"] / sensor["sensorId"] / pollutant / model_name /
                 "error.csv")
-            data.append([REGRESSION_MODELS[model_name], dataframe_errors.iloc[0][error_type]])
+            data.append([REGRESSION_MODELS[model_name], dataframe_errors.iloc[0][error_val]])
 
         dataframe_algorithms = DataFrame(data, columns=["algorithm", pollutant]).dropna()
         if dataframe_algorithms.empty:
@@ -52,11 +52,11 @@ def draw_errors(city: dict, sensor: dict, pollutant: str) -> None:
             ax.text(i, value + 0.5, round(value, 1), fontsize=22, horizontalalignment="center")
 
         ax.set_title(f"{city['siteName']} - {sensor['description']}", fontdict={"size": 22})
-        ax.set_ylabel(error_type, fontsize=22)
+        ax.set_ylabel(error_val, fontsize=22)
         y_lim = dataframe_algorithms[pollutant].max() + 10 - (dataframe_algorithms[pollutant].max() % 10)
         ax.set_ylim(ymin=0, ymax=y_lim)
         pyplot.xticks(dataframe_algorithms.index, dataframe_algorithms["algorithm"], horizontalalignment="center",
                       fontsize=22, rotation=30)
 
         save_plot(fig, pyplot, RESULTS_ERRORS_PATH / "plots" / city["cityName"] / sensor["sensorId"] / pollutant,
-                  error_type)
+                  error_key)
