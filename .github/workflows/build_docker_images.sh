56aa75ab7991270d0e3e4068a130009c94bd29d1
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-04-06 22:34:48 +0200

    Update hash_directory function to support multiple exclusion paths

--- .github/workflows/build_docker_images.sh
+++ .github/workflows/build_docker_images.sh
@@ -7,7 +7,7 @@ SOURCE_DIR="${GIT_REPO_PATH}"
 EXCLUDE_KUBERNETES_DIR="kubernetes"
 SOURCE_DIR_SHA_FILE="${GIT_REPO_PATH}/../${GIT_REPO_NAME}_source_dir_sha"
 
-current_sha=$(hash_directory "${SOURCE_DIR}" "${EXCLUDE_KUBERNETES_DIR}")
+current_sha=$(hash_directory "${SOURCE_DIR}" ".github" "${EXCLUDE_KUBERNETES_DIR}")
 
 if [[ -f "${SOURCE_DIR_SHA_FILE}" ]] && [[ "$(cat "${SOURCE_DIR_SHA_FILE}")" == "${current_sha}" ]]; then
   echo "No changes detected in source directory. Skipping build."
