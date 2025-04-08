6d12c973518e6867b6b8ff3a4087f2622a1e0d50
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-04-08 21:34:44 +0200

    Update MongoDB version and dependencies; comment out Flask API deployment

--- .github/workflows/deploy_kubernetes_resources.sh
+++ .github/workflows/deploy_kubernetes_resources.sh
@@ -10,6 +10,6 @@ echo "Deploying MongoDB first to avoid dependency issues..."
 kubectl apply -f "${KUBERNETES_DIR}/deployment/mongo-deployment.yml"
 kubectl rollout status deployment/mongo -n aqra --watch=true
 
-echo "Deploying Flask API..."
-kubectl apply -f "${KUBERNETES_DIR}/deployment/flask-deployment.yml"
-kubectl rollout status deployment/flask -n aqra --watch=true
+#echo "Deploying Flask API..."
+#kubectl apply -f "${KUBERNETES_DIR}/deployment/flask-deployment.yml"
+#kubectl rollout status deployment/flask -n aqra --watch=true
