cd003ec9a50a33ef1ec5f8928b15c3314626eed0
Aleksandar Trenchevski <atrenchevski@gmail.com>
2025-04-23 17:29:17 +0200

    Use Waitress for production server, update dependencies
    Switched from Flask's built-in server to Waitress for better production-level performance and stability. Updated `numpy` and `starlette` to their latest versions to ensure compatibility and bug fixes. Added `waitress` to development requirements.

--- src/api/app.py
+++ src/api/app.py
@@ -1,6 +1,8 @@
+from waitress import serve
+
 from api.config import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    serve(app, host="127.0.0.1", port=5000)
